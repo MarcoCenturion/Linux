992643044d6c9919a1a21550599963b68206d30c
MarcoCenturion <aulavirtualturismo@gmail.com>
2022-04-28 19:32:17 -0300

    pruebas bash

---
+++ ejemplo_pysimple.py
@@ -0,0 +1,12 @@
+#!/home/marco/Beautifullsoup/bin/python3
+
+import PySimpleGUI as sg
+
+sg.theme('BluePurple')
+
+layout =  [[sg.Text('Ingresar el archivo: ')]
+        ,[sg.Input(key='file')],[sg.OK()],sg.Button('Cancelar')]
+
+
+event,values = sg.Window('Introduccion a PySimpleGUI', layout).read()
+sg.Popup(event, values[0])
