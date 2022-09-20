296af86b0a95fb9a5ce7afd2935dd2f9e71847ea
MarcoCenturion <aulavirtualturismo@gmail.com>
2022-09-20 19:21:25 -0300

    contador de palabras

--- recorrer_csv.py
+++ recorrer_csv.py
@@ -4,9 +4,5 @@ archivo = open("/home/marco/ciclo_2020.txt")
 
 for renglon in archivo: 
     print(renglon)
-    for palabra in renglon:
-        print(type(palabra))
-        print(type(renglon))
-
     sys.stdin.readline()
 
