74c5dc01dbdaff6e5b35c4aeb7fb9004a4cd982b
MarcoCenturion <aulavirtualturismo@gmail.com>
2022-10-24 16:20:09 -0300

    Asciiart con variables

--- imagentoascii.py
+++ imagentoascii.py
@@ -1,3 +1,6 @@
 from image_to_ascii import ImageToAscii
+imagen = input("Indicar la ruta a la imagen")
+nombre = input("Indicar el nombre del archivo .txt a exportar")
 
-ImageToAscii(imagePath="/home/marco/imagenes/cris11.jpg",outputFile="cristia.txt")
+ImageToAscii(imagePath=imagen,outputFile=nombre)
+
