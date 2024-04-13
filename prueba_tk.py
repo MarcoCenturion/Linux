1341f89fee599d3dd1226dab239921a872fd8e4b
MarcoCenturion <aulavirtualturismo@gmail.com>
2024-04-13 14:11:01 -0300

    progama amadeus py tkinter

--- prueba_tk.py
+++ prueba_tk.py
@@ -1,11 +1,12 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+import tkinter as tk
+
+ventana = tk.Tk()
+
+ventana.title("Cotizador Amadeus")
+ventana.geometry("200x300+10+10")
+ventana.configure(bg="red")
+
+ventana.mainloop()
 
 
 
