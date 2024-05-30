be7b40d79b1931889b25838f26ace8c90c92945f
marcocenturion <aulavirtualturismo@gmail.com>
2024-05-30 18:43:03 -0300

    script para cotizar bash amadeus y sabre terminado

--- bash_unificado_Amadeus_Sabre.sh
+++ bash_unificado_Amadeus_Sabre.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 #
 # Aprendiendo a usar sed, cat, awk, cut
 # (c) Marco Centurion
@@ -28,34 +28,56 @@ function renglon(){
 }
 clear
 
+t=$1
+mu=$2
+
 function sabre(){
-ltd=$(cat $1 | grep Purchase | awk -F ' ' '{print $7}')
-tramos=$(cat $1 | grep -e '^ . ' | cut -c 4-9,11-17,19-25,31-37 | awk '{print $0}')
-tarifa=$(cat $1 | grep -e :USD | cut -d '.' -f 1 | awk -F: '{print $2}' | sed 's/USD//g' )
-endos=$(cat $1 | grep -e 'REF')
+ltd=$(cat "$t" | grep Purchase | awk -F ' ' '{print $7}')
+tramos=$(cat "$t" | grep -e '^ . ' | cut -c 4-9,11-17,19-25,31-42 | awk '{print $0}')
+tarifa=$(cat "$t" | grep -e :USD | cut -d '.' -f 1 | awk -F: '{print $2}' | sed 's/USD//g' )
+endos=$(cat "$t" | grep -e 'REF')
 }
 
 function amadeus(){
-ltd=$(cat $1 | grep LAST | awk -F '-' '{print $1}')
-tarifa_usd=$(cat $1 | grep USD | awk 'NR == 1 {print $2}')
-equipaje=$(cat $1 | grep -E '2P|1P|0P'|awk '{print $10}')
-tramos=$(cat $1 | grep -e '^  [1-9]'|cut -c 6-12,15-20,23-29,35-43 | awk '{print $0}')
-tarifa=$(cat $1 | grep -e '^USD' | tail -1 | awk '{print $2}' | cut -d '.' -f 1 )
-endos=$(cat $1 | grep -e 'REF')
+ltd=$(cat "$t" | grep LAST | awk -F '-' '{print $1}')
+tarifa_usd=$(cat "$t" | grep USD | awk 'NR == 1 {print $2}')
+equipaje=$(cat "$t" | grep -E '2P|1P|0P'|awk '{print $10}')
+tramos=$(cat "$t" | grep -e '^  [1-9]'|cut -c 6-12,15-20,23-29,35-43 | awk '{print $0}')
+tarifa=$(cat "$t" | grep -e '^USD' | tail -1 | awk '{print $2}' | cut -d '.' -f 1 )
+endos=$(cat "$t" | grep -e 'REF')
+}
+
+function dibujar(){
+echo -en "\n${blueColour}\nFecha de Cotización Emitida: ${endColour}"
+echo -e "${redColour}$now${endColour}"
+echo -en "\n${blueColour}Ultimo día para emitir: ${endColour}"
+echo -e "${redColour}$ltd${endColour}"
+echo -e "\n${blueColour}Cía|Vuelo|Fecha|Ruta|Sale|Llega${endColour}"
+echo -e "${redColour}$tramos${endColour}"
+echo -en "\n${blueColour}Devoluciones y cambios: ${endColour}\n"
+echo -e "${redColour}$endos${endColour}"
+echo -en "\n${blueColour}Tarifa en Pesos Total por pasajero: USD ${endColour}"
+echo -e "${redColour}$(($tarifa+$mu))${endColour}"
 }
 
 echo -ne "\n${greenColour}Esta cotización es del día: $now${endColour}"
 echo -ne "\n${greenColour}El Markup ingresado es: $2\n ${endColour}"
 
-gds=$(cat $1 | awk 'NR == 1 {print 1}' | cut -c 1,2)
-echo -e "La variable es: $gds"
+gds=$(cat $1 | awk 'NR == 1 {print $1}' | cut -c 1,2)
 
-if [[ "$gds" = WP ]]
-then
-	echo "Ejecutando Sabre"
-elif [[ "$gds" = FP  ]]
-then	
-	echo "Ejecutando Amadeus"
+if [[ $(echo "$gds") = WP ]]; then 
+echo -en "${blueColour}Cotización Sabre: ${endColour}"
+echo -e "${redColour}$endos${endColour}"
+	sabre 
+	dibujar
+elif [[ $(echo "$gds") = FX ]]; then 
+echo -en "${blueColour}Cotización Amadeus: ${endColour}"
+echo -e "${redColour}$endos${endColour}"
+	amadeus
+	dibujar
 else
-	echo "No se ingresó ningún parámetro".
+echo -ne "No se ingresó ningún parámetro".
 fi
+
+
+exit 1
